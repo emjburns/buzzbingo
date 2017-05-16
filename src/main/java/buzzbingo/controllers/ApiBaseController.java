91de1b34540d45cb84a36619ecfeaf0e3b167995
emjburns <emjeburns@gmail.com>
2017-05-16 16:18:58 -0700

    allowing cross origin requests from localhost

--- src/main/java/buzzbingo/controllers/ApiBaseController.java
+++ src/main/java/buzzbingo/controllers/ApiBaseController.java
@@ -1,8 +1,10 @@
 package buzzbingo.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 //@RequestMapping("/v1")
 public abstract class ApiBaseController {
   public static final String API_VERSION = "v1";
