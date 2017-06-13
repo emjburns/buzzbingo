83122bc8901c505d9038d141428540ff196450dd
Emily Burns <emily.burns@M302624PTG8WP.community.veritas.com>
2017-06-13 16:08:09 -0700

    small ui updates

--- ui/src/app/buzzutils.ts
+++ ui/src/app/buzzutils.ts
@@ -2,18 +2,14 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export class BuzzUtils {
-  protocol: string = "http://";
-  hostname: string = "localhost";
-  port: string = "8080";
-  version: string = "v1/";
-
-  cookieKey: string = "username"
+  apiUrl ="http://localhost:8080/v1/";
+  cookieKey: string = "username";
 
   gameboardName(gameName: string, playerName: string): string {
     return gameName + "-" + playerName;
   }
 
   baseURL(): string {
-    return this.protocol + this.hostname + (this.port? ":" + this.port : "") + "/" + this.version;
+    return this.apiUrl;
   }
 }
